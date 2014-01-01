91358ce0aef20b0449c2059aeddfd70015b16c1d
Helen Hood <helen.c.hood@gmail.com>
2014-01-01 16:03:40 -0500

    Updated test to make sure year is 2014 or under

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -5,6 +5,6 @@ describe Car do
   it { should validate_presence_of :year }
   it { should validate_presence_of :mileage }
 
-  it { should validate_numericality_of(:year).greater_than_or_equal_to(1980) }
+  it { should validate_numericality_of(:year).greater_than_or_equal_to(1980).less_than_or_equal_to(Time.new.year) }
   it { should validate_numericality_of :mileage }
 end
