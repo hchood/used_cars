3e6fc9535055bc9c1f24c0b41f48c532bf87aa7b
Helen Hood <helen.c.hood@gmail.com>
2014-01-01 16:57:33 -0500

    Modified car_spec & model b/c customizing year error message broke validates_inclusion test

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,6 +4,6 @@ class Car < ActiveRecord::Base
   validates_presence_of :mileage
 
   validates_numericality_of :year
-  validates_inclusion_of :year, in: 1980..Time.new.year
+  validates_inclusion_of :year, in: 1980..Time.new.year, message: 'must be 1980 or later'
   validates_numericality_of :mileage
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe Car do
-  this_year = Time.new.year
-
   it { should validate_presence_of :color }
   it { should validate_presence_of :year }
   it { should validate_presence_of :mileage }
 
   it { should validate_numericality_of :year }
-  it { should ensure_inclusion_of(:year).in_range(1980..this_year) }
   it { should validate_numericality_of :mileage }
+
+  it 'should reject a car if year is out of range' do
+    next_year = Time.new.year + 1
+
+    car1 = FactoryGirl.build(:car, year: 1970)
+    car2 = FactoryGirl.build(:car, year: next_year)
+
+    expect(car1).to_not be_valid
+    expect(car2).to_not be_valid
+  end
 end
