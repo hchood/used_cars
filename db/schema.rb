535ab1e057977343e07e744509a013e156b6b1e2
Helen Hood <helen.c.hood@gmail.com>
2014-01-01 16:06:34 -0500

    Generated car model & cars table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140101210137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: true do |t|
+    t.string   "color",       null: false
+    t.string   "description"
+    t.integer  "year",        null: false
+    t.integer  "mileage",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
